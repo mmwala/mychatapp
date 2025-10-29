d03f2d7f8b9a4398e4822bf59c2301b07fd9aed4
mmwala <mmsee2007@gmail.com>
2025-10-29 21:46:16 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Local Chat',
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Local Chat')),
+        body: const Center(
+          child: Text(
+            'Hello from Flutter on mobile!',
+            style: TextStyle(fontSize: 20),
+          ),
+        ),
+      ),
+    );
+  }
+}
